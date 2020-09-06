1406c2704183ee396530800855438800c0c70bdc
tiago <seriquevaladares29@gmail.com>
2020-09-06 19:46:10 -0300

    Add collision for pac-man

--- pacman.c
+++ pacman.c
@@ -1,36 +1,107 @@
-#include <stdio.h>
-#include <ncurses.h>
-#include "lib_pacman.c"
+#include "lib_jogo.h"
 
 int main(){
 
     struct labirinto labirinto;
-    /*int seta;*/
+    int direcao, direcaoAnterior, sucesso;
 
-    inicializaJogo();
-
-    if ( !has_colors() ){
-        printf("O terminal não suporta cores\n");
-        finalizaJogo();
-        return -1;
+    if ( !inicializaJogo() ){
+        return 1;
     }
 
-    int maxLinhas, maxColunas;
-    getmaxyx(stdscr, maxLinhas, maxColunas);
+/* ========================================================================== */
     
-    if ( maxLinhas < MAX_LIN || maxColunas < MAX_COL ){
-        printf("O terminal deve ter no minimo %d x %d \n", MAX_LIN, MAX_COL);
-        finalizaJogo();
-        return -1;
-    }
-
+    /**/
+    struct pacman pacman = criaPacman();
+    pacman.posiLin = POSI_LIN_INICIAL;
+    pacman.posiCol = POSI_COL_INICIAL;
+    /**/
+    
+    /*struct fantasma vermelho = criaFantasma();*/
+    
+    /*struct fantasma azul = criaFantasma();*/
+    
+    /*struct fantasma amarelo = criaFantasma();*/
+    
+    /*struct fantasma rosa = criaFantasma();*/
+/* ========================================================================== */
+    
     labirinto = criaLabirinto();
 
+    direcao = direcaoAnterior = KEY_LEFT;
+
+    clear();
     mostraLabirinto(labirinto);
     mostraLayout();
+    mostraPacman(pacman.posiLin, pacman.posiCol);
+    refresh();
 
-    if ( getch() == 'q' )
-        finalizaJogo();
+    while ( pacman.vivo && pacman.vidas > 0){
+
+        if ( pegaTecla() ){
+            direcao = getch();
+        }
+        else {
+            direcao = direcaoAnterior;
+        }
+
+        if ( direcao == 'q' )
+            break;
+
+        switch ( direcao ){
+            case KEY_UP :
+                sucesso = checaColizao(&labirinto, KEY_UP, &pacman);
+                if ( sucesso )            
+                    movePacman(KEY_UP, &pacman);
+                else if ( checaColizao(&labirinto, direcaoAnterior, &pacman) )
+                    movePacman(direcaoAnterior, &pacman);
+                
+                break;
+
+            case KEY_DOWN :
+                sucesso = checaColizao(&labirinto, KEY_DOWN, &pacman);
+                if ( sucesso )            
+                    movePacman(KEY_DOWN, &pacman);
+                else if ( checaColizao(&labirinto, direcaoAnterior, &pacman) )
+                    movePacman(direcaoAnterior, &pacman);
+            
+                break;
+
+            case KEY_LEFT :
+                sucesso = checaColizao(&labirinto, KEY_LEFT, &pacman);
+                if ( sucesso )            
+                    movePacman(KEY_LEFT, &pacman);
+                else if ( checaColizao(&labirinto, direcaoAnterior, &pacman) )
+                    movePacman(direcaoAnterior, &pacman);
+            
+                break;
+
+            case KEY_RIGHT :
+                sucesso = checaColizao(&labirinto, KEY_RIGHT, &pacman);
+                if ( sucesso )       
+                    movePacman(KEY_RIGHT, &pacman);
+                else if ( checaColizao(&labirinto, direcaoAnterior, &pacman) )
+                    movePacman(direcaoAnterior, &pacman);
+                            
+                break;
+
+        }
+
+        if ( sucesso )
+            direcaoAnterior = direcao;
+
+        clear();
+        mostraLabirinto(labirinto);
+        mostraLayout();
+        mostraPacman(pacman.posiLin, pacman.posiCol);
+        usleep(DELAY);
+        refresh();
+        
+        /* usleep() pra dar delay no jogo, talvez ajude com a velocidade  */
+    }
+
+    free(labirinto.matriz);
+    finalizaJogo();
 
     return 0;
 }
